3f7f59052bc5d101b4151aafc231ba21986a6ef3
eremchuk-mp <72536783+eremchuk-mp-8@users.noreply.github.com>
2022-11-26 09:50:59 +0000

    .

--- optics-calc.js
+++ optics-calc.js
@@ -2,7 +2,7 @@ var default_alpha = 60;
 var alpha_height = Math.PI/180*default_alpha;
 
 $("#calc").on('click', function() {
-    var S = Number($("#S").val());
+    var S = Number($("#S").val())*1000;
     var width = Number($("#photo_width").val());
     var height = Number($("#photo_height").val());
     var x = Number($("#x").val());
