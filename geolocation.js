93ef5ee72c6573b1d9f3827db1f70f367aca9770
eremchuk-mp <72536783+eremchuk-mp-8@users.noreply.github.com>
2022-12-09 14:51:53 +0500

    .

--- geolocation.js
+++ geolocation.js
@@ -11,3 +11,7 @@ function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude +
   "<br>Longitude: " + position.coords.longitude;
 }
+
+$("#loc").on('click', function() {
+  getLocation()
+});
