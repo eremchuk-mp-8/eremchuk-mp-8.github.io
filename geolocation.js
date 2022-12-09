1ce4a229bb120edd4a15676cc9c8a6332e213282
eremchuk-mp <72536783+eremchuk-mp-8@users.noreply.github.com>
2022-12-09 06:38:51 +0000

    +geolocation

---
+++ geolocation.js
@@ -0,0 +1,13 @@
+var x = document.getElementById("geo");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
